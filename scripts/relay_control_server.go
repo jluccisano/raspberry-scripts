27012be4fc795f9e993510f634c9905ca33aaa22
Joseph LUCCISANO <joseph.luccisano@gmail.com>
2017-06-30 14:47:57 +0200

    test get

--- scripts/relay_control_server.go
+++ scripts/relay_control_server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+   "os/exec"
    "fmt"
    "github.com/ungerik/go-rest"
 )
@@ -16,6 +17,19 @@ func main() {
 
 	rest.HandleGET("/test", func() string {
 		return "ici!"
+	})
+	
+	rest.HandleGET("/get", func(params url.Values) (string, error) {
+		v := params.Get("relay")
+		if v == "" {
+			return nil, errors.New("Expecting GET parameter 'relay'")
+		}
+		cmd := exec.Command("python",  "relay_control.py", "get", "--relay 1")
+		fmt.Println(cmd.Args)
+		out, err := cmd.CombinedOutput()
+		if err != nil { fmt.Println(err); }
+		fmt.Println(string(out))
+		return "value = " + string(out), nil
 	})
 
         rest.RunServer("0.0.0.0:8080", stopServerChan)
