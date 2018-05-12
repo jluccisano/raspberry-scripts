5daeafc814453496a140b1ea4d23a1bf255273b6
jluccisano <jluccisano@sierrawireless.com>
2018-05-12 13:49:40 +0200

    add http method

--- scripts/sprinkler/server.py
+++ scripts/sprinkler/server.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 
 
 # POST /sprinkler/zone/1?state=1
-@app.route("/sprinkler/zone/<zone>")
+@app.route("/sprinkler/zone/<zone>", methods = ['POST'])
 def set_zone_by_id(zone):
     state = request.args.get('state')
     return "Hello World!" + state
 
 
 # GET /sprinkler/zone/1
-@app.route("/sprinkler/zone/<zone>")
+@app.route("/sprinkler/zone/<zone>",  methods = ['GET'])
 def get_zone_by_id(zone):
     return get_zone(zone)
 
