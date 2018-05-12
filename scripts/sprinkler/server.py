889b837026efa84727af391814c61303a8d44d27
jluccisano <jluccisano@sierrawireless.com>
2018-05-12 14:54:48 +0200

    fix jsonify wrapper

--- scripts/sprinkler/server.py
+++ scripts/sprinkler/server.py
@@ -41,7 +41,7 @@ def get_zone_by_id(zone):
 # GET /sprinkler/zones
 @app.route("/sprinkler/zones",  methods = ['GET'])
 def get_zones():
-    return jsonify(get_all_zones(), status=200, indent=4, sort_keys=True)
+    return jsonify(status=200, indent=4, sort_keys=True, result = get_all_zones())
 
 # POST /sprinkler/reset
 @app.route("/sprinkler/reset", methods = ['POST'])
